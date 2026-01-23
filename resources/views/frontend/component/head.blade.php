466e7f97186950522240d394ef899cc2f0a17f50
chandanv1010 <tuannc.dev@gmail.com>
2026-01-23 19:16:05 +0700

    Fix

--- resources/views/frontend/component/head.blade.php
+++ resources/views/frontend/component/head.blade.php
@@ -47,7 +47,7 @@
 
 {{-- All CSS files are now imported via Vite in app.scss --}}
 {{-- jQuery with defer to prevent render blocking - moved to bottom or use defer --}}
-<script src="{{ asset('frontend/resources/library/js/jquery.js') }}" defer></script>
+<script src="{{ asset('frontend/resources/library/js/jquery.js') }}"></script>
 @vite(['resources/css/app.scss', 'resources/js/app.js'])
 
 {{-- FAQ Schema JSON-LD (for Post, Major, and School detail pages) --}}
